cc6eaa74e791c2c137b5f20ef19741b78415e243
Zwiterion98 <gonzalohernandez2112@gmail.com>
2023-10-23 14:06:02 -0300

    x12

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ if (window.DeviceMotionEvent) {
             posY += accelerationY / 10; // Adjust the factor as needed
 
             // Limit the position to stay within the bounds of the screen
-            posX = Math.min(Math.max(posX, -1), 100);
-            posY = Math.min(Math.max(posY, -1), 100);
+            posX = Math.min(Math.max(posX, -50), 300);
+            posY = Math.min(Math.max(posY, -50), 300);
             document.querySelector("#x").innerHTML = `accX:${accelerationX}`;
             document.querySelector("#y").innerHTML = `accY:${accelerationY}`;
             document.querySelector("#z").innerHTML = `accZ:${accelerationZ}`;
