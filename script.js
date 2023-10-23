692b951490b6d7fb5a15483743f25356bde59f34
Zwiterion98 <gonzalohernandez2112@gmail.com>
2023-10-23 17:10:20 -0300

    dsadsdas

--- script.js
+++ script.js
@@ -2,13 +2,89 @@
 const background = document.getElementById('background');
 
 // Initialize variables for position
-let posX = 50; // initial X position
-let posY = 50; // initial Y position
+let posX = -43.5; // initial X position
+let posY = -33; // initial Y position
+
+let positions = [
+    { value: "si", x: -17, y: 0 },
+    { value: "no", x: -69, y: 0 },
+    { value: "a", x: -6.3, y: -32 },
+    { value: "b", x: -13, y: -25 },
+    { value: "c", x: -18, y: -21 },
+    { value: "d", x: -24, y: -19 },
+    { value: "e", x: -30, y: -17 },
+    { value: "f", x: -36, y: -15 },
+    { value: "g", x: -42, y: -15 },
+    { value: "h", x: -49, y: -17 },
+    { value: "i", x: -55, y: -17 },
+    { value: "j", x: -60, y: -19 },
+    { value: "k", x: -66, y: -20 },
+    { value: "l", x: -72, y: -25 },
+    { value: "m", x: -78, y: -30 },
+    { value: "n", x: -6, y: -50 },
+    { value: "o", x: -11, y: -44 },
+    { value: "p", x: -15, y: -38 },
+    { value: "q", x: -21, y: -35 },
+    { value: "r", x: -28, y: -31 },
+    { value: "s", x: -35, y: -30 },
+    { value: "t", x: -40, y: -30 },
+    { value: "u", x: -46, y: -30 },
+    { value: "v", x: -53, y: -30 },
+    { value: "w", x: -61, y: -32 },
+    { value: "x", x: -69, y: -38 },
+    { value: "y", x: -76, y: -44 },
+    { value: "z", x: -80, y: 51 },
+    { value: "0", x: -66, y: -56 },
+    { value: "1", x: -17, y: 0 },
+    { value: "2", x: -22, y: 0 },
+    { value: "3", x: -28, y: 0 },
+    { value: "4", x: -33, y: 0 },
+    { value: "5", x: -39, y: 0 },
+    { value: "6", x: -44, y: 0 },
+    { value: "7", x: -50, y: 0 },
+    { value: "8", x: -55, y: 0 },
+    { value: "9", x: -61, y: 0 },
+    { value: "adios", x: -44, y: -66 },
+  ];
+  
+  function searchFor(_letter) {
+    // Get the #info element
+    const infoElement = document.querySelector("#info");
+  
+    // Get the position of the target element (specified by _letter)
+    const targetPosition = positions.find((element) => element.value === _letter);
+  
+    if (targetPosition) {
+      // Define the threshold for proximity
+      const proximityThreshold = 1; // Adjust this value as needed
+  
+      // Calculate the distance between #info and the target position
+      const distanceX = targetPosition.x - infoElement.offsetLeft;
+      const distanceY = targetPosition.y - infoElement.offsetTop;
+  
+      // Calculate the angle to rotate the pointer
+      const angle = Math.atan2(distanceY, distanceX);
+  
+      // Check if #info is near the target
+      if (Math.abs(distanceX) <= proximityThreshold && Math.abs(distanceY) <= proximityThreshold) {
+        // Change the border color to green
+        infoElement.style.borderColor = "#00FF00";
+      } else {
+        // Change the border color back to normal
+        infoElement.style.borderColor = "#FF0000";
+      }
+  
+      // Rotate the pointer towards the target position
+      infoElement.style.transform = `rotate(${angle}rad)`;
+    }
+  }
+  
+  
 
 // Check for device motion support
 if (window.DeviceMotionEvent) {
     // Display a permission dialog
-    if (confirm("Do you want to enable motion-based background movement 7?")) {
+    if (confirm("Do you want to enable motion-based background movement 8?")) {
         // Add event listener for device motion
         window.addEventListener('devicemotion', handleMotion);
 
@@ -29,6 +105,7 @@ if (window.DeviceMotionEvent) {
             // Update the background position\
             
             background.style.transform = `translate(${posX}%, ${posY}%)`;
+            searchFor("a");
         }
     } else {
         alert("Motion-based background movement is disabled.");
