033f544a6aa0745791c00961241937ba47c47429
Zwiterion98 <gonzalohernandez2112@gmail.com>
2023-10-24 18:10:34 -0300

    dsadss

--- script.js
+++ script.js
@@ -223,7 +223,9 @@ function setGyro(){
                }
                
            }
-          }
+          }else {
+            alert("Motion-based background movement is disabled.");
+        }
          
       });
     
@@ -234,46 +236,44 @@ function setGyro(){
         if (confirm("Do you want to enable motion-based background movement  13?")) {
             
         
-            // Add event listener for device motion
-            window.addEventListener('devicemotion', handleMotion);
+           
+           // Add event listener for device motion
+           window.addEventListener('devicemotion', handleMotion);
   
-            function handleMotion(event) {
-                const accelerationX = event.accelerationIncludingGravity.x;
-                const accelerationY = event.accelerationIncludingGravity.y;
-                zAcc = event.accelerationIncludingGravity.z;
-                
-                
-                // Calculate the new position based on device motion
-                posX += accelerationX / 10; // Adjust the factor as needed
-                posY += accelerationY / 10; // Adjust the factor as needed
-  
-                // Limit the position to stay within the bounds of the screen
-                posX = Math.min(Math.max(posX, -87), 0);
-                posY = Math.min(Math.max(posY, -66), 0);
+           function handleMotion(event) {
+               const accelerationX = event.accelerationIncludingGravity.x;
+               const accelerationY = event.accelerationIncludingGravity.y;
+               zAcc = event.accelerationIncludingGravity.z;
+               
+               
+               // Calculate the new position based on device motion
+               posX += accelerationX / 10; // Adjust the factor as needed
+               posY += accelerationY / 10; // Adjust the factor as needed
+ 
+               // Limit the position to stay within the bounds of the screen
+               posX = Math.min(Math.max(posX, -87), 0);
+               posY = Math.min(Math.max(posY, -66), 0);
               // Update the background position\
               document.querySelector("#zAcc").innerHTML = `${zAcc}`;
               if(zAcc > 9 && !horizontal){
                 horizontal = true;
                 passScreen();
               }
-              
-                background.style.transform = `translate(${posX}%, ${posY}%)`;
-                
-                let letter = positions[ind].value;
-                if(ind < positions.length){
-                  searchFor(letter);
-                  letter = positions[ind].value;
-                }
-                
+             
+               background.style.transform = `translate(${posX}%, ${posY}%)`;
+               
+               let letter = positions[ind].value;
+               if(ind < positions.length){
+                 searchFor(letter);
+                 letter = positions[ind].value;
+               }
+               
             }
+          }
         } else {
             alert("Motion-based background movement is disabled.");
         }
-    } else {
-        alert("Device motion is not supported in your browser.");
-    }
-  
-  }
+    }  
 }
 
   
