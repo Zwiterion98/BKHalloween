f6631d9e39d6ee7e4a8a716cf16e9d37b9484d27
Zwiterion98 <gonzalohernandez2112@gmail.com>
2023-10-24 12:50:30 -0300

    sads

--- script.js
+++ script.js
@@ -89,32 +89,25 @@ let positions = [
   
   
   let ind = 0;
-
-  if ( location.protocol != "https:" ) {
-    location.href = "https:" + window.location.href.substring( window.location.protocol.length );
+  function askPermission() {
+    if (typeof DeviceMotionEvent.requestPermission === "function") {
+      DeviceOrientationEvent.requestPermission()
+        .then(response => {
+          if (response == "granted") {
+            window.addEventListener(
+              "deviceorientation",
+              onDeviceOrientationChangeEvent,
+              false
+            );
+            location.reload();
+          }
+        })
+        .catch(console.error);
     }
-    function permission () {
-        if ( typeof(  window.DeviceMotionEvent ) !== "undefined" && typeof( window.DeviceMotionEvent.requestPermission ) === "function" ) {
-            // (optional) Do something before API request prompt.
-            DeviceMotionEvent.requestPermission()
-                .then( response => {
-                // (optional) Do something after API prompt dismissed.
-                if ( response == "granted" ) {
-                    window.addEventListener( "devicemotion", (e) => {
-                        // do something for 'e' here.
-                    })
-                }
-            })
-                .catch( console.error )
-        } else {
-            alert( "DeviceMotionEvent is not defined" );
-        }
-    }
-    const btn = document.getElementById( "request" );
-    btn.addEventListener( "click", permission );
-  
+  }
 // Check for device motion support
 if (window.DeviceMotionEvent) {
+  askPermission();
     // Display a permission dialog
     if (confirm("Do you want to enable motion-based background movement  11?")) {
         // Add event listener for device motion
