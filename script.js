92f8f717bafbec57bfb75ad67c8b080a6f6b5676
Zwiterion98 <gonzalohernandez2112@gmail.com>
2023-10-24 17:42:07 -0300

    dsadas

--- script.js
+++ script.js
@@ -215,7 +215,7 @@ function setGyro(){
                 posX = Math.min(Math.max(posX, -87), 0);
                 posY = Math.min(Math.max(posY, -66), 0);
               // Update the background position\
-                document.querySelector("#zAcc").innerHTML = `${posX} ${posY}`;
+                document.querySelector("#zAcc").innerHTML = `${zAcc}`;
                 
                 background.style.transform = `translate(${posX}%, ${posY}%)`;
                 
