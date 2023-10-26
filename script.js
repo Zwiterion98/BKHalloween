b235e460d34a394dba6874a848d0258a19c1299a
Zwiterion98 <gonzalohernandez2112@gmail.com>
2023-10-26 15:47:07 -0300

    das

--- script.js
+++ script.js
@@ -166,44 +166,44 @@ function setGyro(){
   if(OS == "iOS"){
     DeviceMotionEvent.requestPermission().then(response => {
       if (response == 'granted') {
-        passScreen()
+        passScreen();
         
            
-        // Add event listener for device motion
-        window.addEventListener('devicemotion', handleMotion);
-
-        function handleMotion(event) {
-            const accelerationX = event.accelerationIncludingGravity.x;
-            const accelerationY = event.accelerationIncludingGravity.y;
-            zAcc = event.accelerationIncludingGravity.z;
-            
-            
-            
-           // Update the background position\
-           document.querySelector("#zAcc").innerHTML = `${zAcc}`;
-           if(zAcc > 9 && !horizontal){
-             horizontal = true;
-             passScreen();
-           }
-           if(inGame){
-             // Calculate the new position based on device motion
-             posX += accelerationX / 10; // Adjust the factor as needed
-             posY += accelerationY / 10; // Adjust the factor as needed
-
-             // Limit the position to stay within the bounds of the screen
-             posX = Math.min(Math.max(posX, -87), 0);
-             posY = Math.min(Math.max(posY, -66), 0);
-             background.style.transform = `translate(${posX}%, ${posY}%)`;
-            
-             let letter = positions[ind].value;
-             if(ind < positions.length){
-               searchFor(letter);
-               letter = positions[ind].value;
-             }
-           }
-            
-            
-         }
+           // Add event listener for device motion
+           window.addEventListener('devicemotion', handleMotion);
+  
+           function handleMotion(event) {
+               const accelerationX = event.accelerationIncludingGravity.x;
+               const accelerationY = event.accelerationIncludingGravity.y;
+               zAcc = event.accelerationIncludingGravity.z;
+               
+               
+               
+              // Update the background position\
+              document.querySelector("#zAcc").innerHTML = `${zAcc}`;
+              if(zAcc > 9 && !horizontal){
+                horizontal = true;
+                passScreen();
+              }
+              if(inGame){
+                // Calculate the new position based on device motion
+                posX += accelerationX / 10; // Adjust the factor as needed
+                posY += accelerationY / 10; // Adjust the factor as needed
+  
+                // Limit the position to stay within the bounds of the screen
+                posX = Math.min(Math.max(posX, -87), 0);
+                posY = Math.min(Math.max(posY, -66), 0);
+                background.style.transform = `translate(${posX}%, ${posY}%)`;
+               
+                let letter = positions[ind].value;
+                if(ind < positions.length){
+                  searchFor(letter);
+                  letter = positions[ind].value;
+                }
+              }
+               
+               
+            }
        }else {
         alert("Motion-based background movement is disabled.");
       }
