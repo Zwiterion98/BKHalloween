9180361241782fc0e5498d19943312f31b6893fe
Zwiterion98 <gonzalohernandez2112@gmail.com>
2023-10-27 15:04:31 -0300

    dsds

--- script.js
+++ script.js
@@ -272,7 +272,7 @@ function gameManager(_gameStep){
    case 7:
       game.classList.add('hide');
       cuestions.classList.add('hide');
-      if(gano){
+      if(gano == true){
         winner.classList.remove('hide');
         // MOSTRAR QR VICTORIOSO
       }
